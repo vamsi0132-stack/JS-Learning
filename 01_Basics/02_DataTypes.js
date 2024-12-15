e11da8fbccbcc4f8876afc52d47c0bf82fadda7e
vamsi0132-stack <vamsimaragani93@gmail.com>
2024-12-15 16:04:49 +0000

    saved

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all KS code as newwe version
+
+//alert("Hi how are you") // We are using node js not browser, so we will get an error
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Hi how are you")
+
+
+let name = "vamsi"
+let age = 31
+let loggedIn = false
+
+//Premative data types
+// number => 2 to power of 53
+// bigint
+// string => "" or ''
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
