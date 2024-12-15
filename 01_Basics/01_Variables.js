b6840ecfa43005f17ce82ee3199c406a952bdecc
vamsi0132-stack <vamsimaragani93@gmail.com>
2024-12-15 15:48:31 +0000

    change

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,23 @@
+const accountId = 2245689; // variable cannot be changed once delcared 
+let accountEmail = "mvamsiazure@gmail.com"; // can be changed with in the scope scope/block mean {}
+var accountpassword = "001@$Loveumom";
+accountCity = "Kaikaluru";
+let accountState;
+
+
+//accountId = 2 // not allowed 
+
+accountEmail = "vamsimaragani96@gmail.com"
+
+accountpassword = "002@$Loveumom"
+
+accountCity = "Hyderbad"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+beacuse of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
